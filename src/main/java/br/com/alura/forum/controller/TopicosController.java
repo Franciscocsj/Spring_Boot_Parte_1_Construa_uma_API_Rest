dfcd0cdc907547bd20a61919d7cdf1f65764b346
Francisco Junior <francisco.junior@accesstage.com.br>
2019-07-03 19:33:47 -0300

    Aula 3 Criando Entidades JPA

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -4,6 +4,8 @@ package br.com.alura.forum.controller;
 import br.com.alura.forum.controller.dto.TopicoDTO;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,12 +16,19 @@ import java.util.List;
 @Controller
 public class TopicosController {
 
+    @Autowired
+    private TopicoRepository topicoRepository;
+
     @RequestMapping("/topicos")
     @ResponseBody
-    public List<TopicoDTO> lista(){
-        Topico topico = new Topico("Dúvida","Dúvida com Spring",new Curso("Spring","Programacao"));
+    public List<TopicoDTO> lista(String nomeCurso) {
+        if(nomeCurso == null) {
+            List<Topico> topicos = topicoRepository.findAll();
+            return TopicoDTO.converter(topicos);
+        } else {
+            List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+            return TopicoDTO.converter(topicos);
 
-
-        return TopicoDTO.converter(Arrays.asList(topico,topico,topico));
+        }
     }
 }
