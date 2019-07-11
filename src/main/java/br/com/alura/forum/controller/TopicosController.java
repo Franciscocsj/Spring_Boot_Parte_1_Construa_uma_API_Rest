1498f0146031bc3352559777c23ae1540568cae6
Francisco Junior <francisco.junior@accesstage.com.br>
2019-07-11 16:09:43 -0300

    Aula 4 Utilizando Postman

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -2,27 +2,36 @@ package br.com.alura.forum.controller;
 
 
 import br.com.alura.forum.controller.dto.TopicoDTO;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
+@RequestMapping("/topicos")
 public class TopicosController {
 
     @Autowired
     private TopicoRepository topicoRepository;
 
-    @RequestMapping("/topicos")
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    @GetMapping
     @ResponseBody
     public List<TopicoDTO> lista(String nomeCurso) {
-        if(nomeCurso == null) {
+        if (nomeCurso == null) {
             List<Topico> topicos = topicoRepository.findAll();
             return TopicoDTO.converter(topicos);
         } else {
@@ -30,5 +39,15 @@ public class TopicosController {
             return TopicoDTO.converter(topicos);
 
         }
+    }
+
+    @PostMapping
+    public ResponseEntity<TopicoDTO> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
+        Topico topico = form.converter(cursoRepository);
+        topicoRepository.save(topico);
+
+        URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+        return ResponseEntity.created(uri).body(new TopicoDTO(topico));
+
     }
 }
--- src/main/java/br/com/alura/forum/controller/dto/TopicoDTO.java
+++ src/main/java/br/com/alura/forum/controller/dto/TopicoDTO.java
@@ -4,7 +4,6 @@ import br.com.alura.forum.modelo.Topico;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class TopicoDTO {
